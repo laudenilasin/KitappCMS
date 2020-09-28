4eb29133123d7d3f0116c043743d7a8c9332b286
laudenilasin <laudenilasin@gmail.com>
2020-09-28 23:28:15 +0800

    Add null/undefined checking

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 exports.productPage = async (req, res) => {
   console.log('REQ CONTROLLER', req.session)
   if(req.session.username && req.session.username.type == 'SUPER_ADMIN'){
-    const getProducts = await axios.get(`${req.protocol}://${req.get('host')}/api/products`);
+    const getProducts = await axios.get(`${req.protocol}://${req.get('host')}/api/products`); //add null and undefined checking for getProducts and getProducts.data
     res.render('products', {
       term : 'short',
       products: getProducts.data,
