6683b011c97430b992062f03c73f36c7d259f10e
laudenilasin <laudenilasin@gmail.com>
2020-09-28 23:25:47 +0800

    Add null/undefined checkers and remove console log

--- controllers/manage-users.js
+++ controllers/manage-users.js
@@ -2,8 +2,8 @@ const axios = require('axios')
 exports.manageUserPage = async (req, res) => {
   console.log('REQ CONTROLLER', req.session)
   if(req.session.username && req.session.username.type == 'SUPER_ADMIN'){
-    const users = await axios.get(`${req.protocol}://${req.get('host')}/api/user-accounts`);
-    console.log(users.data)
+    const users = await axios.get(`${req.protocol}://${req.get('host')}/api/user-accounts`); //add null and undefined checker for users and users.data
+    console.log(users.data) //remove console log if not needed
     res.render('manage-users', {
       users : users.data,
       user_data : req.session.username
