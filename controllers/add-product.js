6b870af9b82e60fa8e697835fd779dca0868b239
laudenilasin <laudenilasin@gmail.com>
2020-09-28 23:20:06 +0800

    Add null/undefined checker and remove console logs

--- controllers/add-product.js
+++ controllers/add-product.js
@@ -3,8 +3,8 @@ exports.addProductPage = async (req, res) => {
   let availableOrder = [];
   let withoutOrder = [];
   const getProducts = await axios.get(`${req.protocol}://${req.get('host')}/api/products`);
-  console.log('leng',getProducts.data.length )
-  for(let i = 0 ; i < getProducts.data.length; i++){
+  console.log('leng',getProducts.data.length ) //remove console log if not needed.
+  for(let i = 0 ; i < getProducts.data.length; i++){ //add null and undefined checker for getProducts and getProducts.data
     availableOrder.push( parseInt(getProducts.data[i].order) )
   }
   let largest= 0;
@@ -14,15 +14,15 @@ exports.addProductPage = async (req, res) => {
       }
   }
   largest += 1
-  console.log('Largest', largest)
-  console.log('Orders controller', availableOrder)
+  console.log('Largest', largest) //remove console log if not needed.
+  console.log('Orders controller', availableOrder) //remove console log if not needed.
   let available = [];
-  if(getProducts.data[0] != undefined){
+  if(getProducts.data[0] != undefined){ //add null checker for getProducts.data[0]
     available = getProducts.data[0].withoutOrder
   }
   available.push(largest)
-  console.log('ASD', available)
-  available = available.filter((a, b) => available.indexOf(a) === b)
+  console.log('ASD', available) //remove console log if not needed.
+  available = available.filter((a, b) => available.indexOf(a) === b) //add null and undefined checker for available
   if(req.session.username && req.session.username.type == 'SUPER_ADMIN'){
     res.render('add-product', {
       label : 'Add New Product',
