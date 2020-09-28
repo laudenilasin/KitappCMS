af1f06efd90b4a32f028f2daa7f65c8181d9953b
laudenilasin <laudenilasin@gmail.com>
2020-09-28 23:27:02 +0800

    Add null/undefined checking

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 exports.messagePage = async (req, res) => {
-  let messages = await axios.get(`${req.protocol}://${req.get('host')}/api/messages`)
+  let messages = await axios.get(`${req.protocol}://${req.get('host')}/api/messages`) //add null and undefined checkers for messages.data and messages.data.payload
   if(req.session.username && req.session.username.type == 'SUPER_ADMIN'){
     res.render('messages', {
       user_data : req.session.username,
