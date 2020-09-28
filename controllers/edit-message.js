37d3f17438a0a3d5d0414b4888865e6b75d5123f
laudenilasin <laudenilasin@gmail.com>
2020-09-28 23:15:37 +0800

    Add null/undefined checking for messages

--- controllers/edit-message.js
+++ controllers/edit-message.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 exports.messageEditPage = async (req, res) => {
   console.log(req.params)
   console.log(req.protocol)
-  const messages = await axios.post(`${req.protocol}://${req.get('host')}/api/messages/${req.params.id}`);
+  const messages = await axios.post(`${req.protocol}://${req.get('host')}/api/messages/${req.params.id}`); //add null and undefined checking for messages
   if(req.session.username && req.session.username.type == 'SUPER_ADMIN'){
     res.render('edit-messages', {
       messages : messages.data.payload,
